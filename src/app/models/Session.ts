4a3fc55d065517f025d0697ae0fc5f841426b975
Tim383 <67367668+Tim383@users.noreply.github.com>
2020-06-24 14:34:30 +0300

    Add files via upload

---
+++ src/app/models/Session.ts
@@ -0,0 +1,27 @@
+﻿import { Building } from './Building';
+import { Room } from './Room';
+import { Speaker } from './Speaker';
+
+export interface Session {
+    alreadyRegistered: boolean;
+    building: Building;
+    camerasPermitted: number;
+    detailedDescription: string;
+    endTime: Date;
+    id: string;
+    language: number;
+    name: string;
+    recordingsPermitted: number;
+    room: Room;
+    sessionFormat: number;
+    sessionObjectives: string;
+    sessionPreRequisites: string;
+    sessionSummary: string;
+    sessionType: number;
+    startTime: Date;
+    userEligibleToRegister: boolean;
+    speakers: Speaker[];
+    maxCapacity: number;
+    isCapacityRestricted: boolean;
+    registrationCount: number;
+}
